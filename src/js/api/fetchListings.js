243e92b7533c37674cc3eb171789735f111831d2
EM-90 <eirik.michielsen@gmail.com>
2023-11-22 23:38:02 +0100

    fixed url and return ids for listing

--- src/js/api/fetchListings.js
+++ src/js/api/fetchListings.js
@@ -1,13 +1,14 @@
 export async function fetchListings() {
   try {
     const response = await fetch(
-      "https://api.noroff.dev/api/v1/auctions/listings"
+      "https://api.noroff.dev/api/v1/auction/listings"
     );
     const apiResults = await response.json();
 
     apiResults.forEach((listing) => {
       console.log(listing.id);
     });
+    return listing;
   } catch {
     console.log("Error");
   }
