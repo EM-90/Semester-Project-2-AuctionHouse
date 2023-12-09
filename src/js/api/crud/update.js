b6ef53a511daef671e4ce33de2a349abd8e3c369
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-12-09 14:49:04 +0100

    Added update function for avatar

--- src/js/api/crud/update.js
+++ src/js/api/crud/update.js
@@ -1,9 +1,10 @@
 import { load } from "../../storage/index.js";
+import { baseUrl } from "../urls/all-urls.js";
 
-export async function updateListing(listingId, updatedData) {
+export async function update(url, updatedData) {
   try {
     const token = load("token");
-    const response = await fetch(`/api/listings/${listingId}`, {
+    const response = await fetch(url, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -15,4 +16,24 @@ export async function updateListing(listingId, updatedData) {
   } catch (error) {
     console.error("Error updating listing:", error);
   }
+}
+
+export function updateProfileAvatar() {
+  const editForm = document.getElementById("editForm");
+
+  editForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(editForm);
+    let profileAvatarUrl = formData.get("avatar");
+    const profileData = load("token");
+
+    const updatedData = { avatar: profileAvatarUrl };
+
+    const response = await update(
+      `${baseUrl}/profiles/${profileData.name}/media`,
+      updatedData
+    );
+    return response;
+  });
 }
