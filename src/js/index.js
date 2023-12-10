9d76abe76aca629f50c71767d71adfeb44ff1b67
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-12-10 22:44:41 +0100

    Added last route to get the navigation right

--- src/js/index.js
+++ src/js/index.js
@@ -52,6 +52,8 @@ export function initProfilePage() {
 }
 
 export function addItemPage() {
+  if (lastRoute === "addItem") return;
+  lastRoute = "addItem";
   displayAuctionItem();
   setupAuctionItemFormListener();
 }
